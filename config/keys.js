c98d0d5ab7c1d26c88ebb59be4324357dd4ce0a0
Ralph Dela Cruz <34559304+ralphster1998@users.noreply.github.com>
2019-10-26 14:14:42 -0700

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// don't commit this
+
+module.exports = { 
+    googleClientID: '116884826581-p9ruuleh5454edr0rj5tqosfg7m86jah.apps.googleusercontent.com',
+    googleClientSecret: 'XlxfTs0UmiRXcOv0ItW7p3I-', 
+    mongoURI: 'mongodb+srv://p00gz:p00gz@emaily-e4p10.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'dkafljsadkfjksladjfklsjdfklsjadkfljasdkfljg'
+}
